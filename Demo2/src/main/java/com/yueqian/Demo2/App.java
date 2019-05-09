3c293149400a36c64f9ac35f9cdc80d94383a88d
mmUncle <992742958@qq.com>
2019-05-09 11:33:54 +0800

    我也哈哈

--- Demo2/src/main/java/com/yueqian/Demo2/App.java
+++ Demo2/src/main/java/com/yueqian/Demo2/App.java
@@ -11,15 +11,5 @@ public class App
         System.out.println( "Hello World!" );
     }
     //张三写完了
-    void test1() {}
-    //张三再次写完了
-    void test3() {}
-    //李四也写完了
-    void test2() {}
-    //张三在test-banstqack下操作的
-    void test4() {}
-    //master分支
-    void master() {}
-    //master分支
-    void master2() {}
+   
 }
